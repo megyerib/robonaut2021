353c7361bf5dfca5137fd0b1740a88b23456f6ca
Balázs Megyeri <m1eri@users.noreply.github.com>
2020-10-30 17:34:59 +0100

    CRC8 common class added

---
+++ common/CRC/CRC.pro
@@ -0,0 +1,11 @@
+TEMPLATE = app
+CONFIG += console c++11
+CONFIG -= app_bundle
+CONFIG -= qt
+
+SOURCES += \
+        crc8.cpp \
+        main.cpp
+
+HEADERS += \
+        crc8.h
---
+++ common/CRC/main.cpp
@@ -0,0 +1,23 @@
+#include <cstdio>
+#include <cstring>
+#include <cstdint>
+#include "crc8.h"
+
+int main()
+{
+    uint8_t test[] = "Hello!";
+    size_t testLen = strlen((char*)test);
+
+    uint8_t crc = Crc8::GetBlockCrc(test, testLen);
+    printf("%02X\n", crc);
+
+    Crc8 crcCalculator;
+    for (size_t i = 0; i < testLen; i++)
+    {
+        crcCalculator.PushByte(test[i]);
+    }
+    crc = crcCalculator.GetCrc();
+    printf("%02X\n", crc);
+
+    return 0;
+}
